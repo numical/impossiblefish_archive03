8539f8318203a928038cc3f16e3339e8e6f042ad
Mike Evans <mike.evans@numical.com>
2016-01-30 15:51:16 +0000

    commented entry.js

--- entry.js
+++ entry.js
@@ -1,30 +1,38 @@
+// static non-javascript assets for webpack
 require( './styles/impossiblefish.css' );
 
+// static javascript assets using es6 modules (transpiled by babel for webpack) 
 import * as autosizeTank from './scripts/autosizeElement.js';
 import * as reactMenu from './scripts/reactMenu.js';
-
 const behaviours = [ autosizeTank, reactMenu ];         
 
+// main initialisation
 window.onload = function(){
- const gui = { 
-   canvas: $('canvas'),
-   fishtank: $('#fishtank'),
-   menu: $('#menu'),
-   openmenu: $('#openmenu')
- };
- behaviours.forEach( behaviour => {
-  if ( behaviour.init ) {
-   let resizeFn = behaviour.init( gui );
-   if ( resizeFn ) {
-    window.addEventListener( 'resize', resizeFn, false );
-    window.addEventListener('orientationchange', resizeFn, false );
-   }
-  }    
- } );
-};
 
+  // bind to GUI
+  const gui = { 
+    window,
+    canvas: $('canvas'),
+    fishtank: $('#fishtank'),
+    menu: $('#menu')
+  };
+
+  // initialise each behaviour
+  behaviours.forEach( b => { initBehaviour( b, gui ) } );
+}
+
+// nano JQuery!
 function $( selector, container ) {
  return ( container || document ).querySelector( selector );
 }
 
-
+// defines 'behaviour' contract - rather yuk
+function initBehaviour( behaviour, gui ) {
+  if ( behaviour.init ) {
+    let resizeFn = behaviour.init( gui );
+    if ( resizeFn ) {
+      gui.window.addEventListener( 'resize', resizeFn, false );
+      gui.window.addEventListener('orientationchange', resizeFn, false );
+    }
+  }    
+}
