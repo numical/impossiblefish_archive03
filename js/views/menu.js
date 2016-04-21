5e4264814cef675b6d2ae1cf24cac31288b8f94d
Mike Evans <mike.evans@numical.com>
2016-04-21 10:46:18 +0100

    responsive centered menu

--- js/views/menu.js
+++ js/views/menu.js
@@ -3,12 +3,14 @@ import React from 'react'
 const MenuView = React.createClass({
   render () {
     return (
-      <div id='menu' style={this.getStyle()}>
-        <ul>
-          {this.props.items.map((item) => {
-            return <li key={item.display} onClick={() => this.props.fireAction(item.action)}>{item.display}</li>
-          })}
-        </ul>
+      <div id='menu-container'>
+        <div id='menu' style={this.getStyle()}>
+          <ul>
+            {this.props.items.map((item) => {
+              return <li key={item.display} onClick={() => this.props.fireAction(item.action)}>{item.display}</li>
+            })}
+          </ul>
+        </div>
       </div>
     )
   },
