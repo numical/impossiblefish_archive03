574363436bb42f802c6f576149c7326a9605e89b
Mike Evans <mike.evans@numical.com>
2016-05-24 13:09:36 +0100

    quickened menu, slowed fish

--- js/fish/behaviour.js
+++ js/fish/behaviour.js
@@ -6,6 +6,7 @@ import TWEEN from 'tween.js'
 
 const FISH_SCALE = 1
 const FISH_UNIT = 6
+const FEM = FISH_UNIT * FISH_SCALE
 const FISH_ID = new Counter()
 
 export function newFish (fishtank) {
@@ -21,19 +22,11 @@ export function newFish (fishtank) {
     y: initialPos.y,
     rotation: initialPos.rotation,
     tweenPos: initialPos,
-    FEM: FISH_UNIT * FISH_SCALE
+    FEM: FEM
   }
 }
 
 export function incrementFishPosition (fish, fishtank) {
-  /*
-  const x = fish.tweenPos.x > fishtank.size.width
-          ? fish.tweenPos.x - fishtank.size.width
-          : fish.tweenPos.x
-  const y = fish.tweenPos.y > fishtank.size.height
-          ? fish.tweenPos.y - fishtank.size.height
-          : fish.tweenPos.y
-  */
   return Object.assign({}, fish, {
     x: fish.tweenPos.x,
     y: fish.tweenPos.y,
@@ -46,16 +39,29 @@ function createMovement (position, fishtank) {
     x: randomInt(fishtank.size.width),
     y: randomInt(fishtank.size.height)
   }
+  limitMovement(position, finalPos, 100 * FEM)
+  const durations = {
+    rotation: randomInt(500, 1000),
+    movement: randomInt(2000, 4000)
+  }
   const rotate = new TWEEN.Tween(position)
-    .to({rotation: calculateAngle(position, finalPos)}, 1000)
+    .to({rotation: calculateAngle(position, finalPos)}, durations.rotation)
     .easing(TWEEN.Easing.Exponential.Out)
   const move = new TWEEN.Tween(position)
-    .to({x: finalPos.x, y: finalPos.y}, 3000)
+    .to({x: finalPos.x, y: finalPos.y}, durations.movement)
     .easing(TWEEN.Easing.Exponential.Out)
     .onComplete(() => {
       createMovement(position, fishtank)
     })
   rotate.chain(move).start()
+}
+
+function limitMovement (fromPos, toPos, maxDistance) {
+  const distance = Math.hypot(toPos.y - fromPos.y, toPos.x - fromPos.y)
+  if (distance > maxDistance) {
+    toPos.y = fromPos.y + Math.floor((toPos.y - fromPos.y) * maxDistance / distance)
+    toPos.x = fromPos.x + Math.floor((toPos.x - fromPos.x) * maxDistance / distance)
+  }
 }
 
 function calculateAngle (fromPos, toPos) {
