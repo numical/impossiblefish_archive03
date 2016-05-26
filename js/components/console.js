4d48d7a2da39d8d511137e393bcce52da75ec17f
Mike Evans <mike.evans@numical.com>
2016-05-26 16:28:32 +0100

    commands tree working for console

--- js/components/console.js
+++ js/components/console.js
@@ -6,124 +6,105 @@ import { get } from '../util/content.js'
 import View from '../views/console.js'
 import { connect } from 'react-redux'
 
-const NOUNS = {
-  CONSOLE: 'console',
-  FISH: 'fish',
-  TANK: 'tank'
+class Command {
+  constructor (text, action, isApplicable, ...subcommands) {
+    this.text = text
+    this.action = action
+    this.isApplicable = isApplicable || (() => true)
+    this.subcommands = subcommands
+  }
+  isLeaf () {
+    return this.subcommands.length === 0
+  }
 }
-const VERBS = {
-  HELP: 'help',
-  HIDE: 'hide',
-  ADD: 'add',
-  REMOVE: 'remove',
-  PLAY: 'play',
-  PAUSE: 'pause',
-  INFINITE: 'infinite',
-  FINITE: 'finite'
-}
+
 const DELIMITER = ' '
-const NO_FISH = 'There are no fish to remove'
+const COMMANDS = [
+  new Command('help', (props) => displayToConsole(helpOptions(props))),
+  new Command('hide', null, null,
+    new Command('console', hideConsole)
+  ),
+  new Command('add', null, null,
+    new Command('fish', addFish)
+  ),
+  new Command('remove', null, null,
+    new Command('fish', removeFish, (props) => props.fish)
+  ),
+  new Command('pause', pauseAnimation, (props) => props.fish && props.playing),
+  new Command('play', playAnimation, (props) => props.fish && !props.playing),
+  new Command('finite', null, null,
+    new Command('tank', finiteTank, (props) => props.fish && props.infiniteTank)
+  ),
+  new Command('infinite', null, null,
+    new Command('tank', infiniteTank, (props) => props.fish && !props.infiniteTank)
+  )
+]
+
+const helpOptions = (props) => {
+  const options = getApplicableCommands(COMMANDS, props)
+  return get('CONSOLE_HELP_INTRO') + options.sort().join(get('CONSOLE_HELP_DELIMITER'))
+}
+
+const getApplicableCommands = (commands, props) => {
+  return commands
+    .filter((command) => command.isApplicable(props))
+    .reduce((options, command) => {
+      if (command.isLeaf()) {
+        options.push(command.text)
+      } else {
+        const subcommands = getApplicableCommands(command.subcommands, props)
+          .map((subcommand) => command.text + DELIMITER + subcommand)
+        options.push(...subcommands)
+      }
+      return options
+    }, [])
+}
+
+const parseCommand = (text, dispatch, props) => {
+  dispatch(displayToConsole(get('CONSOLE_CURSOR') + text))
+  const command = text.trim().split(DELIMITER).reduce((commands, text) => {
+    const found = findCommand(text, commands, props)
+    if (!found) {
+      return undefined
+    } else if (found.isLeaf()) {
+      return found
+    } else {
+      return found.subcommands
+    }
+  }, COMMANDS)
+  if (command) {
+    dispatch(command.action(props)) // props only used by help action
+  } else {
+    dispatch(displayToConsole(get('CONSOLE_INVALID_COMMAND')))
+  }
+}
+
+const findCommand = (text, commands, props) => {
+  return commands
+    .filter((command) => command.isApplicable(props))
+    .find((command) => command.text === text)
+}
 
 const mapStateToProps = (state) => {
   return Object.assign({}, state.console, {
-    existingFish: state.fishtank.fish.length > 0,
+    fish: state.fishtank.fish.length > 0,
     playing: state.animation.playing,
     infiniteTank: state.fishtank.infinite
   })
 }
 
-// framework would do this by default - but this is more explicit
 const mapDispatchToProps = (dispatch) => {
+  // framework would do this by default - but this is more explicit
   return {dispatch: dispatch}
 }
 
 const mergeProps = (stateProps, dispatchProps, ownProps) => {
   const mergedProps = Object.assign({}, ownProps, stateProps)
   const stateAwareDispatchProps = {
-    processCommand: (command) => parseCommand(command, dispatchProps.dispatch, mergedProps)
+    processCommand: (text) => parseCommand(text, dispatchProps.dispatch, mergedProps)
   }
   return Object.assign({}, mergedProps, stateAwareDispatchProps)
 }
-
-class Command {
-  constructor (command, action, isApplicable, ...subcommands) {
-    this.command = command
-    this.action = action
-    this.applicable = isApplicable || function () {return true}
-    this.subcommands = subcommands
-    this.isLeaf = !this.subcommands
-  }
-  isApplicable (props) {
-    return true
-  }
-}
-
-const commands = [
-  new Command(
-    'help', 
-    (props) => {displayToConsole(buildHelpString(props))}
-  ),
-  new Command(
-    'hide', 
-    null,
-    null,
-    new Command(
-      'console',
-      hideConsole
-    )
-  ),
-  new Command(
-    'add',
-    null,
-    null,
-    new Command(
-      'fish',
-      addFish
-    )
-  ),
-  new Command(
-    'remove',
-    null,
-    null,
-    new Command(
-      'fish',
-      removeFish,
-      (props) => props.existingFish
-    )
-  ),
-  new Command(
-    'pause',
-    pauseAnimation,
-    (props) => props.playing
-  ),
-  new Command(
-    'play',
-    playAnimation,
-    (props) => !props.playing
-  ),
-  new Command(
-    'finite',
-    null,
-    null,
-    new Command(
-      'tank',
-      finiteTank,
-      (props) => props.infiniteTank
-    )
-  ),
-  new Command(
-    'infinite',
-    null,
-    null,
-    new Command(
-      'tank',
-      infiniteTank,
-      (props) => !props.infiniteTank
-    )
-  )
-]
-
-
 
 export default connect(
   mapStateToProps,
